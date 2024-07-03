95786bfe076f4e676fe2e4724b550c324200a13a
LuisReartez <luisreartez55@gmail.com>
2024-07-02 21:43:26 -0300

    se realizo ejercicio8

---
+++ js/ejercicio8.js
@@ -0,0 +1,11 @@
+// ejercicio 8
+
+let numero = parseInt(prompt("Ingrese un numero"));
+
+if(numero % 2 == 0){
+    document.write("El numero ingresado es divisible por 2 -"+numero)
+}
+
+else{
+    document.write("No es divisible pór 2");
+}
